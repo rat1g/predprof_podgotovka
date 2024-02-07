8c8511cb1df0252320bbc29b3d15eb183bdf6599
rat1g <rester7.gg@gmail.com>
2024-02-07 17:44:19 +0300

    2e

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+class Studentiki:
+    id = 0
+    full_name = ''
+    id_project = 0
+    clas = ''
+    score = 0
 
+students = []
+f = open('students.csv')
+skip = f.readline()
+j = 0
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for i in f:
+    s = i.split(',')
+    if int(s[3][:-1]) == 10:
+        students.append(Studentiki())
+        students[j].full_name = s[1]
+        students[j].score = s[4]
+        j += 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for i in range(len(students)):
+    j = 1
+    t = students[i]
+    while j > 0 and students[j-1].score > t.score:
+        students[j] = students[j-1]
+        j -= 1
+    students[j] = t
+
+print(f'''
+10 класс:
+1 место: {students[2].full_name[0]}.{students[2].full_name.split()[0]}
+2 место: {students[4].full_name[0]}.{students[4].full_name.split()[0]}
+3 место: {students[6].full_name[0]}.{students[6].full_name.split()[0]}
+''')
